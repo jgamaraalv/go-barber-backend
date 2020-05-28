02e6b7bfd581c9d46b9409bb1ca58240e360626e
João Guilherme do Amaral Vequiato <joao.vequiato@S2IT.CORP>
2020-05-28 16:44:54 -0300

    Implements password cryptography

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface ExecuteUserDTO {
@@ -23,7 +25,13 @@ class CreateUserService {
       throw new Error('Email address already used');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
 
