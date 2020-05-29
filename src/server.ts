ac9fbda421c65437322224adcdfe5599861cd29c
João Guilherme do Amaral Vequiato <joao.vequiato@S2IT.CORP>
2020-05-29 15:13:55 -0300

    Implements avatar upload

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import express from 'express';
 
 import './database';
 import routes from './routes';
+import { tmpFolder } from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(tmpFolder));
 app.use(routes);
 
 app.listen(3333, () => {
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import User from '../models/User';
+import { tmpFolder } from '../config/upload';
+
+interface ExecuteUserAvatarDTO {
+  user_id: string;
+  avatar_file_name: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({
+    user_id,
+    avatar_file_name,
+  }: ExecuteUserAvatarDTO): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(tmpFolder, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatar_file_name;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
